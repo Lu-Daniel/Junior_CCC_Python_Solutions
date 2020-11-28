bba3da7e42bb43369b8f2f99944bc95c57eeb89e
Dorajev <56607493+Dorajev@users.noreply.github.com>
2020-11-28 17:28:49 -0500

    Update J3.py

--- 2020/J3.py
+++ 2020/J3.py
@@ -6,12 +6,12 @@ x_list = []
 y_list = []
 
 for i in range(N):
-    coordinate = input("Enter the coordinate in (x, y): ")
+    coordinate = input("Enter the coordinate in (x,y): ")
     comma = coordinate.find(",")
     x_val = coordinate[:comma]
     x_list.append(int(x_val))
     x_list.sort()
-    y_val = coordinate[comma+2:]
+    y_val = coordinate[comma+1:]
     y_list.append(int(y_val))
     y_list.sort()
 
@@ -20,7 +20,7 @@ x2 = str(x_list[N-1] +1)
 y1 = str(y_list[0] - 1)
 y2 = str(y_list[N-1] + 1)
 
-print(x1+", "+y1)
-print(x2+", "+y2)
+print(x1+","+y1)
+print(x2+","+y2)
 
 
